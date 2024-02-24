ae6ebe619d473d145253136af089ef09e385ca66
Siavash Safi <git@hosted.run>
2024-02-24 18:48:05 +0100

    fix: drop io/ioutil
    Drop deprecated `io/ioutil` package.
    
    References:
    - https://pkg.go.dev/io/ioutil
    
    Signed-off-by: Siavash Safi <git@hosted.run>

--- jwt/config.go
+++ jwt/config.go
@@ -17,7 +17,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -86,7 +85,7 @@ func (js jwtSource) Token() (*oauth2.Token, error) {
 		return nil, fmt.Errorf("oauth2: cannot fetch token: %v", err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1<<20))
+	body, err := io.ReadAll(io.LimitReader(resp.Body, 1<<20))
 	if err != nil {
 		return nil, fmt.Errorf("oauth2: cannot fetch token: %v", err)
 	}
