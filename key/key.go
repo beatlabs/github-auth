10ee97d59cb862d0576ae39a14f02629403a6f8c
Siavash Safi <git@hosted.run>
2024-02-24 18:51:56 +0100

    fix: drop io/ioutil (#50)
    Drop deprecated `io/ioutil` package.
    
    References:
    - https://pkg.go.dev/io/ioutil
    
    Signed-off-by: Siavash Safi <git@hosted.run>

--- key/key.go
+++ key/key.go
@@ -3,13 +3,13 @@ package key
 import (
 	"crypto/rsa"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 // FromFile loads a private key from the provided path and parses it.
 // The private key is returned if parsing succeeds.
 func FromFile(path string) (*rsa.PrivateKey, error) {
-	key, err := ioutil.ReadFile(path)
+	key, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read private key: %s", err.Error())
 	}
